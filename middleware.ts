ff6e65faac4773979c9c286decd79bd86bac5cad
nickss-123 <Nabesamis0@gmail.com>
2026-09-21 17:27:08 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -61,6 +61,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|webp|gif)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|manifest.json|sw.js|offline.html|icons/|.*\\.(?:svg|png|jpg|jpeg|webp|gif)$).*)',
   ],
 }
